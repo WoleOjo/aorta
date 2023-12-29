2e6899d4c7545247a18dccf7c86b2f88d6678c02
Wole Ojo <woleojo@Woles-MacBook-Air.local>
2023-12-29 18:27:18 +0100

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
-		<div className="h-[20px] bg-white absolute bottom-0 w-full flex items-center justify-center">
-			<h4 className="text-xs ">&copy;2024 Aorta Capital</h4>
+		<div className="h-[100px] bg-black absolute bottom-0 w-full flex items-center justify-center">
+			<h4 className="text-xs text-white">&copy;2024 <span className="text-bold">Aorta</span> Capital</h4>
 		</div>
 	)
 }
